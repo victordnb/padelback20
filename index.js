0ac47d21edaac18e915cf2b27a6c6e3cfcbc998b
victordnb <victordnb@gmail.com>
2024-02-19 01:12:31 +0100

    login, no compara bien las psw

--- index.js
+++ index.js
@@ -4,11 +4,13 @@ const playersRouter = require('./routes/players');
 const registerRouter = require('./routes/register');
 const app = express();
 const port = process.env.PORT || 3000;
+const loginRouter = require('./routes/login');
 
 app.use(cors());
 app.use(express.json());
 app.use('/api/players', playersRouter);
 app.use('/api/register', registerRouter);
+app.use('/api/login', loginRouter);
 
 app.listen(port, () => {
   console.log(`Servidor corriendo en http://localhost:${port}`);
